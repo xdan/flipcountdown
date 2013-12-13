ebbaa5cfe0a9f7c24077207b1ba30cabee9523f1
Valeriy <skoder@ya.ru>
2013-12-13 19:59:58 +0600

    New major version 3.0.0
    Now the plugin can display not only the time. but any number. it can be
    used as a counter or timer

--- doc.tpl
+++ doc.tpl
@@ -11,9 +11,42 @@ $(function(){
 })
 </script>
 <header>
-	<h1>jquery.flipCountDown Plugin <small>jQuery plugin flip count down timer or clock</small></h1>
+	<h1>jquery.flipCountDown Plugin <small>jQuery plugin flip count down timer,clock or counter</small></h1>
 	<p>Unusual retro clock count down plugin for jQuery <a href="https://github.com/xdan/flipcountdown/">Source</a> or <a href="https://github.com/xdan/flipcountdown/archive/master.zip">Download</a></p>
 </header>
+<hr>
+<h2>Until the new year left</h2>
+<table style="border:0px;">
+	<tr>
+		<td style="width:110px;text-align:center;"><strong>Days</strong></td>
+		<td style="width:120px;text-align:center;"><strong>Hours</strong></td>
+		<td style="width:110px;text-align:center;"><strong>Minutes</strong></td>
+		<td style="width:120px;text-align:center;"><strong>Seconds</strong></td>
+	</tr>
+	<tr>
+		<td colspan="4"><span id="new_year"></span></td>
+	</tr>
+</table>
+<script>
+$(function(){
+	var NY = Math.round((new Date('1/01/2014 00:00:01')).getTime()/1000);
+	$('#new_year').flipcountdown({	
+		size:'lg',
+		tick:function(){
+			var nol = function(h){
+				return h>9?h:'0'+h;
+			}
+			var	range  	= NY-Math.round((new Date()).getTime()/1000),
+				secday = 86400, sechour = 3600,
+				days 	= parseInt(range/secday),
+				hours	= parseInt((range%secday)/sechour),
+				min		= parseInt(((range%secday)%sechour)/60),
+				sec		= ((range%secday)%sechour)%60;
+			return nol(days)+' '+nol(hours)+' '+nol(min)+' '+nol(sec);
+		}
+	});
+});
+</script>
 <h2>Fast start</h2>
 <p>Add in html page</p><pre><code data-language="html">&lt;script type=&quot;text/javascript&quot; src=&quot;jquery.min.js&quot;&gt;&lt;/script&gt;
 &lt;script type=&quot;text/javascript&quot; src=&quot;jquery.flipcountdown.js&quot;&gt;&lt;/script&gt;
@@ -71,8 +104,60 @@ $(function(){
 <p>- speed animate flip digit (multiply 6 must by less than 1000) default 100</p>
 <pre><code data-language="javascript">$(&quot;#flipcountdownbox5&quot;).flipcountdown({speedFlip:50});</code></pre>
 <div id="flipcountdownbox5"></div>
+<hr>
 <p>- set time</p>
-<pre><code data-language="javascript">$('#flipcountdownbox6').flipcountdown({time:function(){
+<pre><code data-language="javascript">$('#flipcountdownbox6').flipcountdown({tick:function(){
 	return new Date('5.10.2012 12:34:23');
 }});</code></pre>
 <div id="flipcountdownbox6"></div>
+<hr>
+<p>- Counter</p>
+<pre><code data-language="javascript">$(function(){
+	var i = 1;
+	$(&#39;#retroclockbox_counter&#39;).flipcountdown({
+		tick:function(){
+			return i++;
+		}
+	});
+})</code></pre>
+<div id="retroclockbox_counter"></div>
+<script>
+$(function(){
+	var i = 1;
+	$('#retroclockbox_counter').flipcountdown({
+		tick:function(){
+			return i++;
+		}
+	});
+})
+</script>
+<hr>
+<p>- Float</p>
+<pre><code data-language="javascript">$(&#39;#retroclockbox_float&#39;).flipcountdown({size:&#39;lg&#39;,tick:22.3434});</code></pre>
+<div id="retroclockbox_float"></div>
+<script>
+$(function(){
+	$('#retroclockbox_float').flipcountdown({size:'lg',tick:22.3434});
+});
+</script>
+
+<hr>
+<p> - Until The New Year Example</p>
+<pre><code data-language="javascript">$(function(){
+ var NY = Math.round((new Date(&#39;1/01/2014 00:00:01&#39;)).getTime()/1000);
+ $(&#39;#new_year&#39;).flipcountdown({	
+	size:&#39;lg&#39;,
+	tick:function(){
+		var nol = function(h){
+			return h>9?h:&#39;0&#39;+h;
+		}
+		var	range  	= 	NY-Math.round((new Date()).getTime()/1000),
+			secday	= 	86400, sechour = 3600,
+			days	= 	parseInt(range/secday),
+			hours	= 	parseInt((range%secday)/sechour),
+			min	= 	parseInt(((range%secday)%sechour)/60),
+			sec	= 	((range%secday)%sechour)%60;
+		return nol(days)+&#39; &#39;+nol(hours)+&#39; &#39;+nol(min)+&#39; &#39;+nol(sec);
+	}
+ });
+});</code></pre>
--- jquery.flipcountdown.js
+++ jquery.flipcountdown.js
@@ -1,180 +1,183 @@
 /** 
- * @preserve jQuery flipcountdown plugin v2.0.1
+ * @preserve jQuery flipcountdown plugin v3.0.0
  * @homepage http://xdsoft.net/jqplugins/flipcountdown/
  * (c) 2013, Chupurnov Valeriy.
  */
 (function($){
 jQuery.fn.flipCountDown = jQuery.fn.flipcountdown = function( _options ){
 	var default_options = {
-			showHour:true,
-			showMinute:true,
-			showSecond:true,
-			am:false,
-			showMinute:true,
-			showSecond:true,
-			am:false,
-			tzoneOffset:0,
-			speedFlip:100,
-			time:function(){
-				return new Date();
-			},
-			size:'md'
+			showHour	:true,
+			showMinute	:true,
+			showSecond	:true,
+			am			:false,
+
+			tzoneOffset	:0,
+			speedFlip	:60,
+			period		:1000,
+			tick		:function(){
+							return new Date();
+						},
+			autoUpdate	:true,
+			size		:'md'
 		},
 		sizes = {
 			lg:77,
 			md:52,
 			sm:35,
 			xs:24
-		}, 
-		div = function(val, by){
-			return (val - val % by) / by;
-		}
-		
+		},
 		createFlipCountDown = function( $box ){
-			var flipcountdown 		= $('<div class="xdsoft_flipcountdown"></div>'),
-				hourBoxLeft 	= $('<div class="xdsoft_digit"></div>'),
-				hourBoxRight 	= $('<div class="xdsoft_digit"></div>'),
-				
-				minuteBoxLeft 	= $('<div class="xdsoft_digit"></div>'),
-				minuteBoxRight 	= $('<div class="xdsoft_digit"></div>'),
-				
-				secondBoxLeft 	= $('<div class="xdsoft_digit"></div>'),
-				secondBoxRight 	= $('<div class="xdsoft_digit"></div>'),
-				
-				separator1 		= $('<div class="xdsoft_digit xdsoft_separator"></div>'),
-				separator2 		= $('<div class="xdsoft_digit xdsoft_separator"></div>'),
+			var flipcountdown 	= $('<div class="xdsoft_flipcountdown"></div>'),
+				clearex 		= $('<div class="xdsoft_clearex"></div>'),	 
 				
 				options = $.extend({},default_options),
 				
 				timer = 0,
 				
-				heightDigit = 52,
-				
-				current = {
-					"h1": -1,
-					"h2": -1,
-					"m1": -1,
-					"m2": -1,
-					"s1": -1,
-					"s2": -1
+				_animateRange = function( box,a,b ){
+					_animateOne( box,a,(a>b&&!(a==9&&b==0))?-1:1,!(a==9&&b==0)?Math.abs(a-b):1 );
 				},
 				
-				_getmt =  function ( up ){
-					return (-(((up)?up-1:9)*6-1)*heightDigit);
+				_animateOne = function( box,a,arrow,range ){
+					if( range<1 )
+						return;
+	
+					_setMargin(box,-(a*6*sizes[options.size]+1),1,arrow,function(){
+						_animateOne(box,a+arrow,arrow,range-1);
+					},range);
 				},
-				
-				_setMargin = function( box, mt, rec){
-					if( rec==1 )
-						box.css('background-position-y',mt+'px');
-					box.css('background-position-y',(mt-heightDigit)+'px' );
-					if( rec<6 )
+				_setMargin = function( box, marginTop, rec, arrow,callback,range){
+					if( marginTop<=-sizes[options.size]*60 )
+						marginTop = -1;
+					box.css('background-position-y',marginTop+'px' );
+					if( rec<=6 ){
 						setTimeout(function(){
-							_setMargin(box,mt-heightDigit,++rec);	
-						},options.speedFlip);
+							_setMargin(box, marginTop-arrow*sizes[options.size], ++rec, arrow, callback,range);	
+						},parseInt(options.speedFlip/range));
+					}else
+						callback();
 				},
-				
-				_calcTime = function(){
-					var now = options.time(),
-						h = (now.getHours()+options.tzoneOffset)%(options.am?12:24),
-						h1 = div(h,10),
-						h2 = h%10,
-						m1 = div(now.getMinutes() , 10),
-						m2 = now.getMinutes() % 10,
-						s1 = div(now.getSeconds() , 10),
-						s2 = now.getSeconds() % 10;
+				blocks = [],
+				_typeCompare	= 	function ( a,b ){
+					return 	a&&b&&(
+								(a==b)||
+								(/^[0-9]+$/.test(a+''+b))||
+								(/^[:.\s]+$/.test(a+''+b))
+							);
+				},
+				_generate = function( chars ){
+					if( !(chars instanceof Array) || !chars.length )
+						return false;
+					for( var i = 0, n = chars.length;i<n;i++ ){
+						if( !blocks[i] ){
+							blocks[i] = $('<div class="xdsoft_digit"></div>');
+							clearex.before(blocks[i]);
+						}
+						if( blocks[i].data('value')!=chars[i] ){
+							if( !_typeCompare(blocks[i].data('value'),chars[i]) ){
+								blocks[i]
+									.removeClass('xdsoft_separator')
+									.removeClass('xdsoft_dot');
+								switch( chars[i] ){
+									case ':':blocks[i].addClass('xdsoft_separator');break;
+									case '.':blocks[i].addClass('xdsoft_dot');break; 
+									case ' ':blocks[i].addClass('xdsoft_space');break; 
+								}
+							}
+							if( !isNaN(chars[i]) ){
+								//_setMargin(blocks[i],_getmt(parseInt(chars[i])),1);
+								var old = parseInt(blocks[i].data('value')), 
+									ii = parseInt(blocks[i].data('i')),
+									crnt = parseInt(chars[i]);
+								if( isNaN(old)||i!=ii ){
+									old = (crnt-1)<0?9:crnt-1;
+								}
+								_animateRange(blocks[i],old,crnt);
+							}
+							blocks[i].data('value',chars[i]);
+							blocks[i].data('i',i);
+						}
+					}
+					if( blocks.length>chars.length ){
+						for(;i<blocks.length;i++ ){
+							blocks[i][0].parentNode.removeChild(blocks[i][0]);
+							delete blocks[i];
+						}
+						blocks.splice(chars.length);
+					}
 					
-					if( options.showHour && h2 != current.h2 ){
-						_setMargin(hourBoxRight,_getmt(h2),1);
-						current.h2 = h2;
-					}
-					if( options.showHour && h1 != current.h1 ){
-						_setMargin(hourBoxLeft,_getmt(h1),1);
-						current.h1 = h1;
-					}
-					if( options.showMinute && m2 != current.m2 ){
-						_setMargin(minuteBoxRight,_getmt(m2),1);
-						current.m2 = m2;
-					}
-					if( options.showMinute && m1 != current.m1 ){
-						_setMargin(minuteBoxLeft,_getmt(m1),1);
-						current.m1 = m1;
-					}
+				},
+				counter = 0,
+				_calcMoment = function(){
+					var value = '1',chars = [];
+					if(options.tick)
+						value = (options.tick instanceof Function)?options.tick.call($box,counter):options.tick;
 					
-					if ( options.showSecond && s2 != current.s2 ){
-						_setMargin(secondBoxRight,_getmt(s2),1);
-						current.s2 = s2;
-					}
-					if ( options.showSecond && s1 != current.s1 ){
-						_setMargin(secondBoxLeft,_getmt(s1),1);
-						current.s1 = s1;
+					if( typeof value!=='undefined' ){
+						switch( value.constructor ){
+							case Date:
+								var h = (value.getHours()+options.tzoneOffset)%(options.am?12:24);
+		
+								if( options.showHour ){
+									chars.push(parseInt(h/10));
+									chars.push(h%10);
+								}
+										
+								if( options.showHour && (options.showMinute || options.showSecond) )
+									chars.push(':');
+								
+								if( options.showMinute ){
+									chars.push(parseInt(value.getMinutes()/10));
+									chars.push(value.getMinutes() % 10);
+								}
+								
+								if( options.showMinute && options.showSecond )
+									chars.push(':');
+								
+								if( options.showSecond ){
+									chars.push(parseInt(value.getSeconds()/10));
+									chars.push(value.getSeconds() % 10);
+								}
+							break;
+							case String:
+								chars = value.replace(/[^0-9\:\.\s]/g,'').split('');
+							break;
+							case Number:
+								chars = value.toString().split('');
+							break;
+						}
+						_generate(chars);
 					}
 				};
 				
 			flipcountdown
-				.append(hourBoxLeft)
-				.append(hourBoxRight)
-				.append(separator1)
-				.append(minuteBoxLeft)
-				.append(minuteBoxRight)
-				.append(separator2)
-				.append(secondBoxLeft)
-				.append(secondBoxRight)
-				.append('<div class="xdsoft_clearex"></div>')
-				
+				.append(clearex)
 				.on('xdinit.xdsoft',function(){
-					if( !(options.showHour || options.showMinute || options.showSecond) )
-						options.showHour = options.showMinute = options.showSecond = true;
-						
-					flipcountdown
-						.find('.xdsoft_digit,.xdsoft_separator')
-							.hide();
-					
-					if( options.showHour ){
-						hourBoxLeft.show();
-						hourBoxRight.show();
-					}
-							
-					if( options.showHour && (options.showMinute || options.showSecond) )
-						separator1.show();
-					
-					if( options.showMinute ){
-						minuteBoxLeft.show();
-						minuteBoxRight.show();
-					}
-					
-					if( options.showMinute && options.showSecond )
-						separator2.show();
-					
-					if( options.showSecond ){
-						secondBoxLeft.show();
-						secondBoxRight.show();
-					}
-					
 					clearInterval(timer);
-					
-					timer = setInterval(_calcTime,1000);
-					
-					_calcTime();
+					if( options.autoUpdate )
+						timer = setInterval( _calcMoment,options.period );
+					_calcMoment();
 				});
 				
-			$box.setOptions = function( _options ){
+			$box.data('setOptions',function( _options ){
 				options = $.extend({},options,_options);
 				if( !sizes[options.size] )
 					options.size = 'lg';
-				heightDigit = sizes[options.size];
 				flipcountdown
 					.addClass('xdsoft_size_'+options.size)
 					.trigger('xdinit.xdsoft');
-			};
+			});
 			$box.append(flipcountdown);
 		};	
 	return this.each(function(){
 		var $box = $(this);
-		if( !$box.hasClass('xdsoft') ){
+		if( !$box.data('setOptions') ){
 			$box.addClass('xdsoft')
 			createFlipCountDown($box);
 		}
-		$box.setOptions(_options);
+		$box.data('setOptions')&&
+			$.isFunction($box.data('setOptions'))&&
+				$box.data('setOptions')(_options);
 	});
 }
 })(jQuery);
